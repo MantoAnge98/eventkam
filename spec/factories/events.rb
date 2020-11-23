5d0858795b348ce3822bbfe83c1d9cd9d9738bd5
AngeManto98 <koutikaangemarie@gmail.com>
2020-11-23 11:49:09 +0000

    Add rspec and default image

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :event do
-    
+    title {'event1'}
+    image {'image.png'}
+    content {'Sed ut perspiciatis unde
+      omnis iste natus error sit voluptatem
+      accusantium doloremque laudantium,
+      totam rem aperiam4'}
+    date_start {'2020-11-27'}
+    date_end {'2020-11-30'}
+
+    association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "user1" }
+    avatar {"image.png"}
+    email { "user@gmail.com" }
+    password{ "000000" }
+    password_confirmation{ "000000" }
+  end
+
+  factory :second_user , class: User do
+    name { "user2" }
+    avatar {"image.png"}
+    email { "user1@gmail.com" }
+    password{ "000000" }
+    password_confirmation{ "000000" }
   end
 end
