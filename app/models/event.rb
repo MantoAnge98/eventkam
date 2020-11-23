b37658fafcfe79f2c6cb1bb61e09f73fc1afd7e6
AngeManto98 <koutikaangemarie@gmail.com>
2020-11-23 10:51:08 +0000

    Add Model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,46 @@
 class Event < ApplicationRecord
+  mount_uploader :image, ImageUploader 
+
+
+  belongs_to :user, foreign_key: 'organizer_id'
+  has_many :participants,  dependent: :destroy
+  has_many :parts, through: :participants, foreign_key: 'participants_id', dependent: :destroy
+
+  scope :past_events, -> { where('date_start < ?', Time.now.end_of_day) }
+  scope :future_events, -> { where('date_start >= ?', Time.now.end_of_day) }
+
+  validates :title , presence: true
+  validates :content, presence: true, :length => { :maximum => 500 } 
+
+  validates_presence_of :date_start, :date_end 
+  validate :date_end_is_after_date_start
+
+  has_many :labellings, dependent: :destroy
+  has_many :labels, through: :labellings
+
+
+  def total_particpants
+    0
+  end
+
+  paginates_per 4
+
+  def image_thumbnail
+    if image.present?
+      image.url
+    else
+      '/image.png'
+    end
+  end
+
+
+  private
+  def date_end_is_after_date_start
+    if date_end < date_start
+      errors.add(:date_end, "Cannot be before the start date")
+    end
+  end
+  
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  mount_uploader :avatar, AvatarUploader
+
+  validates :name , presence: true , uniqueness: true
+
+  has_many :events, foreign_key: 'organizer_id', dependent: :destroy
+  has_many :participants, foreign_key: 'participants_id', class_name: 'Participant',  dependent: :destroy
+  has_many :participate, through: :participants,  dependent: :destroy
+
+
+  def avatar_thumbnail
+    if avatar.present?
+      avatar.url
+    else
+      '/default-avatar.png'
+    end
+  end
+
+  paginates_per 4
+  
 end
